145cdc36bf30e452732349e30f66f1098fa2b535
gin-gorm <534645465@qq.com>
2022-05-25 15:06:01 +0800

    评论api

--- routers/router.go
+++ routers/router.go
@@ -43,29 +43,29 @@ func Router() {
 		auth.GET("admin/users", v1.GetUsers)
 		auth.PUT("user/:id", v1.EditUser)      //TODO bug存在 密码 手机号等修改  必须提供name role字段
 		auth.DELETE("user/:id", v1.DeleteUser) // 软删除
-		////修改密码
+		//修改密码
 		auth.PUT("admin/changepw/:id", v1.ChangeUserPassword) //TODO 存在bug  该方法需要对传入的数据进行加密解密
-		//// 分类模块的路由接口
+		// 分类模块的路由接口
 		auth.GET("admin/category", v1.GetCate)
 		auth.POST("category/add", v1.AddCate)
 		auth.PUT("category/:id", v1.EditCate)
 		auth.DELETE("category/:id", v1.DeleteCate)
-		//// 文章模块的路由接口
+		// 文章模块的路由接口
 		auth.GET("admin/article/info/:id", v1.GetArtInfo) // GetArtInfo 查询单个文章信息
 		auth.GET("admin/article", v1.GetArt)              // GetArt 查询文章列表   TODO bug 无法查询
 		auth.POST("article/add", v1.AddArticle)           // AddArticle 添加文章
 		auth.PUT("article/:id", v1.EditArt)               // EditArt 编辑文章  TODO bug 无法修改
 		auth.DELETE("article/:id", v1.DeleteArt)          // DeleteArt 删除文章 TODO bug 不存在的删除没有正确提示
-		//// 上传文件
+		// 上传文件
 		auth.POST("upload", v1.UpLoad)
-		//// 更新个人设置
+		// 更新个人设置
 		auth.GET("admin/profile/:id", v1.GetProfile) // GetProfile 获取个人信息设置
 		auth.PUT("profile/:id", v1.UpdateProfile)    // UpdateProfile 更新个人信息设置
-		//// 评论模块
-		//auth.GET("comment/list", v1.GetCommentList)
-		//auth.DELETE("delcomment/:id", v1.DeleteComment)
-		//auth.PUT("checkcomment/:id", v1.CheckComment)
-		//auth.PUT("uncheckcomment/:id", v1.UncheckComment)
+		// 评论模块
+		auth.GET("comment/list", v1.GetCommentList)       // GetCommentList 后台查询评论列表
+		auth.DELETE("delcomment/:id", v1.DeleteComment)   // DeleteComment 删除评论
+		auth.PUT("checkcomment/:id", v1.CheckComment)     // CheckComment 通过审核
+		auth.PUT("uncheckcomment/:id", v1.UncheckComment) // UncheckComment 撤下评论审核
 	}
 
 	/*
@@ -77,28 +77,28 @@ func Router() {
 		router.POST("user/add", v1.Register)   //TODO 账户创建对于软删除的手机号存在bug
 		router.GET("user/:id", v1.GetUserInfo) //TODO 账户信息查询只提供name role 差手机号
 		router.GET("users", v1.GetUsers)       //查询单个用户信息
-		//
-		//// 文章分类信息模块
+
+		// 文章分类信息模块
 		router.GET("category", v1.GetCate)
 		router.GET("category/:id", v1.GetCateInfo)
-		//
-		//// 文章模块
+
+		// 文章模块
 		router.GET("article", v1.GetArt)              // GetArt 查询文章列表  TODO bug 无法查询
 		router.GET("article/list/:id", v1.GetCateArt) // GetCateArt 查询分类下的所有文章
 		router.GET("article/info/:id", v1.GetArtInfo) // GetArtInfo 查询单个文章信息
-		//
-		//// 登录控制模块
+
+		// 登录控制模块
 		router.POST("login", v1.Login)
 		router.POST("loginfront", v1.LoginFront)
-		//
-		//// 获取个人设置信息
+
+		// 获取个人设置信息
 		router.GET("profile/:id", v1.GetProfile) // GetProfile 获取个人信息设置
-		//
-		//// 评论模块
-		//router.POST("addcomment", v1.AddComment)
-		//router.GET("comment/info/:id", v1.GetComment)
-		//router.GET("commentfront/:id", v1.GetCommentListFront)
-		//router.GET("commentcount/:id", v1.GetCommentCount)
+
+		// 评论模块
+		router.POST("addcomment", v1.AddComment)               // AddComment 新增评论
+		router.GET("comment/info/:id", v1.GetComment)          // GetComment 获取单个评论信息
+		router.GET("commentfront/:id", v1.GetCommentListFront) // GetCommentListFront 展示页面显示评论列表
+		router.GET("commentcount/:id", v1.GetCommentCount)     // GetCommentCount 获取评论数量
 	}
 	server := viper.GetString("server.port")
 	r.Run(":" + server)
